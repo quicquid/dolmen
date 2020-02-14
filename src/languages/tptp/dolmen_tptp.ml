72a4379964943a5d34fd4e0542b8bf4f8ee9e2d1
Guillaume Bury <guillaume.bury@gmail.com>
2020-02-14 17:26:11 +0100

    Add alias for versioned parser

--- src/languages/tptp/dolmen_tptp.ml
+++ src/languages/tptp/dolmen_tptp.ml
@@ -1,6 +1,9 @@
 
 (* This file is free software, part of dolmen. See file "LICENSE" formore information *)
 
+(* Alias the sub-libraries *)
+module V6_3_0 = Dolmen_tptp_v6_3_0
+
 (* Include the latest version *)
 include Dolmen_tptp_v6_3_0
 
